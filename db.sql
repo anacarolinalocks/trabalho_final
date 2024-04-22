3ebdb92ee518765768cf3183218f37193cf01252
ANA CAROLINA LOCKS GOTTERT <03440521095@senacrs.edu.br>
2024-04-22 11:45:50 -0300

    API]

--- db.sql
+++ db.sql
@@ -5,7 +5,7 @@ USE young_reader;
 DROP TABLE young_user;
 
 CREATE TABLE young_user(
-	id INT PRIMARY KEY,
+	id INT PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(255),
     data_de_nascimento DATE,
     email VARCHAR(255) NOT NULL UNIQUE,
@@ -13,17 +13,21 @@ CREATE TABLE young_user(
 );
 
 SELECT * FROM young_user;
+
+DROP TABLE young_categorias;
 	
 CREATE TABLE young_categorias(
-	id_book INT,
+	id_book INT PRIMARY KEY AUTO_INCREMENT,
     classificação INT,
     genero VARCHAR(255)
 );
 
 SELECT * FROM young_categorias;
 
+DROP TABLE young_favoritos;
+
 CREATE TABLE young_favoritos(
-	id_book INT,
+	id_book INT PRIMARY KEY AUTO_INCREMENT,
     estrelas INT
 );
 
