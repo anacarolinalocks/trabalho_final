efa4f720e44dd06fad9afb574ffa8f20f7c6ef33
ANA CAROLINA LOCKS GOTTERT <03440521095@senacrs.edu.br>
2024-07-30 11:53:00 -0300

    login e cadastro

---
+++ API/src/controller/userController.js
@@ -0,0 +1,12 @@
+const connection = require('/config/db');
+const dotenv = require('dotenv').config();
+async function storeUser(req, res){
+
+    const parms = Array(
+        req.body.name,
+        req.body.email,
+        req.body.
+    );
+
+    cosnt query = 
+}
